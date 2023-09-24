7b005aae143984ab5339c41e1a120f0380f75c60
ps-woramet <ps.woramet@gmail.com>
2023-09-24 00:16:13 -0700

    fix category model

--- model/Category.js
+++ model/Category.js
@@ -1,32 +1,32 @@
-import mongoose from 'mongoose';
+//category schema
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema(
-    {
-        name: {
-            type: String,
-            require: true,
-        },
-        user:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'user',
-            required: true,
-        },
-        image:{
-            type: String,
-            require: true,
-        },
-        products: [
-            {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product',
-        }
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
     ],
-    },{
-        timestamps: true
-    }
+  },
+  { timestamps: true }
 );
 
-const Category = mongoose.model('Category', CategorySchema);
+const Category = mongoose.model("Category", CategorySchema);
 
 export default Category;
