25896237cc2fa341807e588f58e153c284697196
ps-woramet <ps.woramet@gmail.com>
2023-09-24 00:25:27 -0700

    fix model user

--- model/User.js
+++ model/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const UserShema = new Schema({
+const UserSchema = new Schema({
     fullname: {
         type: String,
         required: true,
@@ -66,5 +66,5 @@ const UserShema = new Schema({
 }, {timestamps: true,})
 
 //compile the schema to model
-const User = mongoose.model("User", UserShema);
+const User = mongoose.model("User", UserSchema);
 export default User;
