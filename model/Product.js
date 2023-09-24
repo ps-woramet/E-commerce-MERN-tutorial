ac751d9a8306129308ab8d5cc08ee831b7459943
ps-woramet <ps.woramet@gmail.com>
2023-09-24 00:39:45 -0700

    fix product model

--- model/Product.js
+++ model/Product.js
@@ -1,91 +1,100 @@
 //product schema
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema({
+const ProductSchema = new Schema(
+  {
     name: {
-        type: String,
-        require: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        require: true,
+      type: String,
+      required: true,
     },
     brand: {
-        type: String,
-        require: true,
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        ref: "Category",
-        requirede: true,
+      type: String,
+      ref: "Category",
+      required: true,
     },
     sizes: {
-        type: [String],
-        enaum: ["s","M","L","XL","XXL"],
-        require: true,
+      type: [String],
+      enum: ["S", "M", "L", "XL", "XXL"],
+      required: true,
     },
     colors: {
-        type: [String],
-        require: true,
+      type: [String],
+      required: true,
     },
+
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        require: true,
-        ref: "User",
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
+
     images: [
-        {
-            type: String,
-            required: true,
-        },
+      {
+        type: String,
+        required: true,
+      },
     ],
+
     reviews: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Review",
-        },
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
     ],
+
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
+
     totalQty: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     totalSold: {
-        type: Number,
-        required: true,
-        default: 0,
+      type: Number,
+      required: true,
+      default: 0,
     },
-},
-{
+  },
+  {
     timestamps: true,
-    toJSON: {virtuals: true},
-});
+    toJSON: { virtuals: true },
+  }
+);
 //Virtuals
 //qty left
-ProductSchema.virtual('qtyLeft').get(function(){
-    const product = this;
-    return product.totalQty - product.totalSold;
-})
+ProductSchema.virtual("qtyLeft").get(function () {
+  const product = this;
+  return product.totalQty - product.totalSold;
+});
 //Total rating
-ProductSchema.virtual('totalReviews').get(function(){
-    const product = this;
-    return product?.reviews?.length;
+ProductSchema.virtual("totalReviews").get(function () {
+  const product = this;
+  return product?.reviews?.length;
 });
 //average Rating
-ProductSchema.virtual('averageRating').get(function(){
-    let ratingsTotal = 0;
-    const product = this;
-    product?.reviews?.forEach((review)=>{
-        ratingsTotal += review?.rating;
-    })
-    //calc average rating
-    const averageRating = Number(ratingsTotal / product?.reviews?.length).toFixed(1);
-    return averageRating;
+ProductSchema.virtual("averageRating").get(function () {
+  let ratingsTotal = 0;
+  const product = this;
+  product?.reviews?.forEach((review) => {
+    ratingsTotal += review?.rating;
+  });
+  //calc average rating
+  const averageRating = Number(ratingsTotal / product?.reviews?.length).toFixed(
+    1
+  );
+  return averageRating;
 });
-
 const Product = mongoose.model("Product", ProductSchema);
+
 export default Product;
